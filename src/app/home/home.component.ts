3eaf02e32cc125d779804c8ffaa648b655869c5f
Olena <72007221+Oleva-abr@users.noreply.github.com>
2024-02-26 00:17:26 +0200

    add buttons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,13 +4,14 @@ import { Product, Products } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
 import { PaginatorModule } from 'primeng/paginator';
-import { FormsModule } from '@angular/forms';
+
 import { EditPopupComponent } from '../components/edit-popup/edit-popup.component';
 
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule, PaginatorModule, EditPopupComponent],
+  imports: [ProductComponent, CommonModule, PaginatorModule, EditPopupComponent, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -25,6 +26,18 @@ export class HomeComponent {
 
   displayEditPopup: boolean = false;
   displayAddPopup: boolean = false;
+
+  toggleEditPopup(product: Product) {
+    this.selectedProduct = product;
+    this.displayEditPopup = true;
+  }
+
+  toggleDeletepopup(product: Product) { }
+
+
+  toggleAddPopup() {
+    this.displayAddPopup = true;
+  }
 
   selectedProduct: Product = {
     id: 0,
